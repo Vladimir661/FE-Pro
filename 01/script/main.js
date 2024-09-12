7046292b83b35177063f8ac811299700a298a0af
Vladimir661 <xphigota@gmail.com>
2024-09-12 21:01:08 +0300

    main

---
+++ 01/script/main.js
@@ -0,0 +1,78 @@
+// alert('Hello world! from separete file');
+var uservalue = 123;
+
+// let const
+const KEYVALUE = 1233445;
+
+let Name = 'Max';
+let name = 'Tom';
+
+// Name  не дорівнює name
+// let user Name = 'Test' error;
+// let user-Name user+Name = 'Test' error;
+// let user+Name = 'Test' error;
+
+let userNameValue = 'Test';
+let UserNameValue = 'Test';
+let user_name = 'Test';
+const USERNAME = 'test';
+
+// let let = 'test' error
+// let const = 'test' error
+let falseValue = 'test';
+
+// let a = 1;
+// let b = 2;
+// let c = a+b;
+
+let firstValue = 1;
+let secondValue = 2;
+
+
+firstValue = 'User Age 34';
+
+
+
+let summResult = firstValue+secondValue;
+
+// String 
+let myStringValue = 'String value';
+let anotherStringValue = "String value";
+
+// Number
+
+let myNumber = 1;
+let floatNmber = 1.345;
+let infinityValue = Infinity;
+let nanValue = NaN; // not a number
+
+console.log(myNumber);
+console.log(floatNmber);
+console.log(infinityValue);
+console.log(nanValue);
+
+console.log('My Number nanValue:', firstValue);
+
+document.write(myStringValue + '<br>');
+document.write(myNumber + '<br>');
+
+let myStringTest = '5';
+let myTestNumber = 5;
+let resultValue;
+
+resultValue = myStringTest + myTestNumber // 55
+console.log('String + number = ', resultValue);
+let minusOperation = myTestNumber - myStringTest;
+console.log('String - number = ', minusOperation);
+
+let divideOperation = myStringTest / myTestNumber;
+console.log('String / number = ', divideOperation);
+
+let multiplyOperation = myTestNumber * myStringTest;
+console.log('String / number = ', multiplyOperation);
+
+//Boolean
+
+let trueValue = true;
+let falseValueExample = false;
+
