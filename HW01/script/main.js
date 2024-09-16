2475ed2ae772e3ba1ec9e3584efffa7ddc5cfafd
Vladimir661 <xphigota@gmail.com>
2024-09-16 16:00:01 +0300

    main

---
+++ HW01/script/main.js
@@ -0,0 +1,24 @@
+let usetNickname = 'MRXssss';
+
+let userScore = 6696;
+
+let age = 20;
+console.log(age);
+
+age = age + 5;
+console.log(age);
+
+let isStudent = true;
+console.log(isStudent);
+
+isStudent = false;
+console.log(isStudent);
+
+let item = 10;
+console.log(item);
+
+item = "ten";
+console.log(item);
+
+item = true;
+console.log(item);
