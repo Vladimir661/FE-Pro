19326f7e902942941bff5203f1d60179d135d006
Vladimir661 <xphigota@gmail.com>
2024-10-14 21:13:21 +0300

    main

---
+++ HW09/netflix/script/main.js
@@ -0,0 +1,54 @@
+//TASK 1
+const rating = 3;
+const stars = document.querySelectorAll('.star i');
+
+stars.forEach((star, index) => {
+    if (index < rating) {
+        star.classList.add('fa-solid', 'yellow-star');
+        star.classList.remove('fa-regular', 'white-star');
+    } else {
+        star.classList.add('fa-regular', 'white-star');
+        star.classList.remove('fa-solid', 'yellow-star');
+    }
+});
+//TASK 2
+const genres = ['Drama', 'Thriller', 'Supernatural'];
+const genreContainer = document.querySelector('.genre');
+
+genreContainer.innerHTML = '';
+
+genres.forEach((genre, index) => {
+    const genreElement = document.createElement('span');
+    genreElement.textContent = genre;
+    genreContainer.appendChild(genreElement);
+    if (index < genres.length - 1) {
+        genreContainer.appendChild(document.createTextNode(' | '));
+    }
+});
+//TASK 3
+const sliderImages = [
+    'img/Br-Ba.png',
+    'img/Rain.png',
+    'img/Year.png',
+    'img/Money.png',
+    'img/Squid.png'
+];
+
+let currentImageIndex = 0;
+const sliderContainer = document.querySelector('.images');
+
+function showImages() {
+    sliderContainer.innerHTML = '';
+    const imgElement = document.createElement('img');
+    imgElement.src = sliderImages[currentImageIndex];
+    sliderContainer.appendChild(imgElement);
+}
+
+function swapImages() {
+    currentImageIndex = (currentImageIndex + 1) % sliderImages.length;
+    showImages();
+}
+
+setInterval(swapImages, 3000);
+
+showImages();
