650a9d7073b8d43db89613197cbf2a953657875f
Vladimir661 <xphigota@gmail.com>
2024-10-28 00:22:27 +0200

    main

--- HW13/script/main.js
+++ HW13/script/main.js
@@ -26,16 +26,16 @@ function showData() {
 }
 //Task 5
 const email = document.querySelector('#email');
-    const password = document.querySelector('#password');
-    const submitBtn = document.querySelector('#submitBtn');
+const password = document.querySelector('#password');
+const submitBtn = document.querySelector('#submitBtn');
 
-    function validateForm() {
-      const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value);
-      const passwordValid = /^[A-Za-z\d]{8,}$/.test(password.value) &&
+function validateForm() {
+    const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value);
+    const passwordValid = /^[A-Za-z\d]{8,}$/.test(password.value) &&
                       /[A-Z]/.test(password.value) &&
                       /\d/.test(password.value);
       submitBtn.disabled = !(emailValid && passwordValid);
-    }
+}
 
-    email.addEventListener('input', validateForm);
-    password.addEventListener('input', validateForm);
+email.addEventListener('input', validateForm);
+password.addEventListener('input', validateForm);
